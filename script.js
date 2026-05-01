d58c9dc1658d340a12d52176015cded4071f621c
Vishal Yadav <ydvishal89@gmail.com>
2026-05-01 10:08:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ Employee.prototype.constructor = Employee;
 // Add jobGreet method
 Employee.prototype.jobGreet = function () {
   console.log(
-    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    `Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`
   );
 };
 
