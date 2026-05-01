3995e2d5883a5a148d127f1e0bf1b6e1a7e23948
Vishal Yadav <ydvishal89@gmail.com>
2026-05-01 10:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
@@ -17,7 +17,7 @@ Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function () {
   console.log(
-    `Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
   );
 };
 
