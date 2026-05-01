e946e220d0fcb3d193542cae7458e670f4a5cb41
Vishal Yadav <ydvishal89@gmail.com>
2026-05-01 10:04:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-// complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Adding method to Person prototype
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+function Employee(name, age, jobTitle) {
+  // Call parent constructor
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Inherit prototype from Person
+Employee.prototype = Object.create(Person.prototype);
+
+// Fix constructor reference
+Employee.prototype.constructor = Employee;
+
+// Add jobGreet method
+Employee.prototype.jobGreet = function () {
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
+};
 
 // Do not change code below this line
 window.Person = Person;
